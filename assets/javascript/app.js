fc0ee1577fff636a472071b7299feec814c0a3f6
Dean Mamnev <deanmamnev@gmail.com>
2018-03-14 17:50:30 -0500

    barebones are done

---
+++ assets/javascript/app.js
@@ -0,0 +1,120 @@
+//timer
+
+setTimeout(function(){
+    totalscore = 0;
+        //if Die Hard question is answered correctly
+        if ($("#diehard").prop("checked")){
+  
+            totalscore++;
+            console.log("button works!" + totalscore);
+            $("#score").text(totalscore + "/6");
+        }
+    
+        //Judas Priest question
+        if ($("#judaspriest").prop("checked")){
+      
+            totalscore++;
+            console.log("button works!" + totalscore);
+            $("#score").text(totalscore + "/6");
+        }
+    
+        //Earthworm Jim
+        if ($("#ewj").prop("checked")){
+      
+            totalscore++;
+            console.log("button works!" + totalscore);
+            $("#score").text(totalscore + "/6");
+        }
+    
+        //Star Trek
+        if ($("#startrek").prop("checked")){
+      
+            totalscore++;
+            console.log("button works!" + totalscore);
+            $("#score").text(totalscore + "/6");
+        }
+    
+        //Car Spoiler
+        if ($("#fnf").prop("checked")){
+      
+            totalscore++;
+            console.log("button works!" + totalscore);
+            $("#score").text(totalscore + "/6");
+        }
+    
+        //Mounted Horsemen
+        if ($("#ww2").prop("checked")){
+      
+            totalscore++;
+            console.log("button works!" + totalscore);
+            $("#score").text(totalscore + "/6");
+        }
+    alert("Time's Up!  Your score is: "+totalscore);
+  }, 90000);
+
+
+
+//button click functions
+$("#done").on("click",
+// console.log("button works!")
+function donebutton(){
+    totalscore = 0;
+    $("#score").text(totalscore);
+    console.log("button works!" + totalscore);
+    
+
+    //if Die Hard question is answered correctly
+    if ($("#diehard").prop("checked")){
+  
+        totalscore++;
+        console.log("button works!" + totalscore);
+        $("#score").text(totalscore + "/6");
+    }
+
+    //Judas Priest question
+    if ($("#judaspriest").prop("checked")){
+  
+        totalscore++;
+        console.log("button works!" + totalscore);
+        $("#score").text(totalscore + "/6");
+    }
+
+    //Earthworm Jim
+    if ($("#ewj").prop("checked")){
+  
+        totalscore++;
+        console.log("button works!" + totalscore);
+        $("#score").text(totalscore + "/6");
+    }
+
+    //Star Trek
+    if ($("#startrek").prop("checked")){
+  
+        totalscore++;
+        console.log("button works!" + totalscore);
+        $("#score").text(totalscore + "/6");
+    }
+
+    //Car Spoiler
+    if ($("#fnf").prop("checked")){
+  
+        totalscore++;
+        console.log("button works!" + totalscore);
+        $("#score").text(totalscore + "/6");
+    }
+
+    //Mounted Horsemen
+    if ($("#ww2").prop("checked")){
+  
+        totalscore++;
+        console.log("button works!" + totalscore);
+        $("#score").text(totalscore + "/6");
+    }
+
+    //perfect score
+    if(totalscore == 6){
+        alert("You Win!")
+    }
+
+
+});
